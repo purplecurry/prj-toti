c64db42bde8a9af6dee2af0f10314ba456cd1d30
ppcry <05mmhb@gmail.com>
2026-03-20 17:59:51 +0900

    timer.py 유저 인증 확인 부분 변경(추후 변경 가능), 탬플릿 todo

--- timer.py
+++ timer.py
@@ -12,14 +12,22 @@ from db import get_db_session, users
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
+async def get_current_user(request: Request, db: AsyncSession = Depends(get_db_session)):
+    user_id = request.session.get("id")
+    if not user_id:
+        return None
+    result = await db.execute(select(users).where(users.id == user_id))
+    user = result.scalars().first()
+    return user
 
 @router.get("/timer")
-async def read_root(request:Request, db: AsyncSession=Depends(get_db_session)):
-    result = await db.execute(select(users).where(users.id == request.session.get("id")))
-    user = result.scalars().first()
-
-    focust_time = user.default_focus_time if user else 25
-    break_time = user.default_break_time if user else 5
+async def timer(request:Request, current_user=Depends(get_current_user),db: AsyncSession=Depends(get_db_session)):
+    
+    focust_time = await current_user.default_focus_time if current_user else 25
+    break_time = await current_user.default_break_time if current_user else 5
 
     return templates.TemplateResponse("timer.html", {"request":request, "focus_time":focust_time, "break_time":break_time,})
 
+@router.get("/timer/memo")
+async def read_memo(request:Request, db: AsyncSession=Depends(get_db_session)):
+    
