df4b4a43d3438077c062fddbbddefb893ac9c4a6
purplecurry <05mmhb@gmail.com>
2026-03-28 00:09:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from fastapi import FastAPI, APIRouter, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from contextlib import asynccontextmanager
+from fastapi.templating import Jinja2Templates
 import ai_service, my_calendar, db, timer, user, stats
 
-# 1. 템플릿(HTML) 폴더 설정 (에러 해결!)
-templates = Jinja2Templates(directory="templates")
 
 @asynccontextmanager
 async def app_life_span(app: FastAPI):
-    async with db.engine.begin() as conn:
-        await conn.run_sync(db.Base.metadata.create_all)
-    yield
+    async with db.engine.begin() as conn:                  # 종료하면 자동으로 닫힌다.
+        await conn.run_sync(db.Base.metadata.create_all)   # 동기 실행 ==> 비동기 실행.
+    yield    
 
 app = FastAPI(lifespan=app_life_span)
 
-# 라우터 연결
 app.include_router(timer.router)
-app.include_router(my_calendar.router)
+# app.include_router(my_calendar.router)
+# app.include_router(ai_service.router)
 app.include_router(user.router)
 app.include_router(stats.router)
+app.include_router(stats.stats_router)
 
-# 2. 첫 화면 설정 (index.html 띄우기)
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/timer")
